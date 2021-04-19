25f582e62e12aee9e2c2ba0c4ab3e247b5b39eaf
choisaelim <choisaelim@gmail.com>
2021-04-19 23:35:38 +0900

    JpaRepository 상속 Interface로 변경

--- jpa-sample/src/main/java/com/example/jpasample/repository/ItemRepository.java
+++ jpa-sample/src/main/java/com/example/jpasample/repository/ItemRepository.java
@@ -1,5 +1,9 @@
 package com.example.jpasample.repository;
 
-public class ItemRepository {
-  
+import com.example.jpasample.domain.Item;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
 }
