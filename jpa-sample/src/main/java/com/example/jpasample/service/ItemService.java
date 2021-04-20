4bdf58e473901b9ea8399276203b69f5c1733c70
choisaelim <choisaelim@gmail.com>
2021-04-20 23:05:20 +0900

    item 데이터 로딩 함수 추가

--- jpa-sample/src/main/java/com/example/jpasample/service/ItemService.java
+++ jpa-sample/src/main/java/com/example/jpasample/service/ItemService.java
@@ -1,5 +1,25 @@
 package com.example.jpasample.service;
 
+import java.util.List;
+
+import com.example.jpasample.domain.Item;
+import com.example.jpasample.repository.ItemRepository;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class ItemService {
-  
+    private final ItemRepository itemRepository;
+
+    public List<Item> getItem() {
+        return itemRepository.findAll();
+    }
+
+    public Item getItem(Long id) {
+        return itemRepository.getOne(id);
+    }
+
 }
